649b689a346b0e77f2fff723d90a375b2c27624b
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-14 12:57:46 -0300

    Update carrinho.js

--- carrinho.js
+++ carrinho.js
@@ -1,3 +1,16 @@
+var umaPropaganda = function() {
+	var propagandas = ["O que acha de comprar uma motocicleta?",
+						"O que acha de comprar uma lancha?",
+						"O que acha de comprar uma bicicleta?",
+						"O que acha de comprar um carro?"];
+	var posicao = Math.floor(propagandas.length * Math.random());
+	var texto = propagandas[posicao];
+	var tr = $("<tr>").addClass("propaganda").append($("<td>"));
+	tr.find("td").attr("colspan", 6).text(texto);
+
+	return tr;
+}
+
 var atualizaDados = function() {
 	var carrinhos = $(".carrinho");
 	carrinhos.each(function() {
@@ -39,6 +52,11 @@ var aposCarregamento = function() {
 	atualizaDados();
 	$(".undo").click(undo);
 	$(".remove-item").click(removeItem);
+	$(".carrinho").each(function() {
+		$(this).find("tr:nth-child(3n)").each(function(){
+			umaPropaganda().insertAfter($(this));
+		});	
+	});	
 }
 
 $(aposCarregamento); // será chamado após a página estiver carregada
