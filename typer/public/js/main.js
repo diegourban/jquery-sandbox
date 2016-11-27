1df3723283d99a198e37a5109fc54f5880fc1379
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-11-27 19:24:07 -0200

    implementing game over

--- typer/public/js/main.js
+++ typer/public/js/main.js
@@ -10,3 +10,15 @@ campo.on('input', function() {
 	var qtdPalavras = conteudo.split(/\S+/).length - 1;
 	$('#contador-palavras').text(qtdPalavras);
 });
+
+var tempoRestante = $('#tempo-digitacao').text();
+campo.one('focus', function() {
+	var cronometroId = setInterval(function() {
+		tempoRestante--;
+		$('#tempo-digitacao').text(tempoRestante);
+		if(tempoRestante < 1) {
+			campo.attr('disabled', 'disabled');
+			clearInterval(cronometroId);
+		}
+	}, 1000);
+});
